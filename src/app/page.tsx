fcc2202e8a0daf8260e951776e7d44009c5a735c
Petar Markovic <petarmarkovic@Petars-MacBook-Pro.local>
2025-01-24 15:09:02 +0100

    chore: new try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,18 @@ import CtaOverview from "@/modules/landing-page/CtaOverview";
 import FAQ from "@/modules/landing-page/faq/FAQ";
 import FunctionalityOverview from "@/modules/landing-page/functionality/FunctionalityOverview";
 import HeroSection from "@/modules/landing-page/hero-section/HeroSection";
-import RatingSwiper from "@/modules/landing-page/ratings/RatingSwiper";
+// import RatingSwiper from "@/modules/landing-page/ratings/RatingSwiper";
 
-const Home = () => {
-  return (
-    <GeneralLayout>
-      <HeroSection />
-      <ApplicationOverview />
-      <FunctionalityOverview />
-      <RatingSwiper />
-      <CtaOverview />
-      <ContactUs />
-      <FAQ />
-    </GeneralLayout>
-  );
-};
+const Home = () => (
+  <GeneralLayout>
+    <HeroSection />
+    <ApplicationOverview />
+    <FunctionalityOverview />
+    {/* <RatingSwiper /> */}
+    <CtaOverview />
+    <ContactUs />
+    <FAQ />
+  </GeneralLayout>
+);
 
 export default Home;
