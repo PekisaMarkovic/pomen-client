ec9bb087dc1855f7ad6f296a029f1f51bde5cae7
Petar Markovic <petarmarkovic@Petars-MacBook-Pro.local>
2025-02-08 17:45:34 +0100

    chore: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -39,7 +39,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.5
     },
     {
-      url: `${host}/blog`,
+      url: `${host}/blogs`,
       changeFrequency: "yearly",
       priority: 0.5
     },
