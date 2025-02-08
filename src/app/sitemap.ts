364b223a2d3fc9f8a3510819202f652450831049
Petar Markovic <petarmarkovic@Petars-MacBook-Pro.local>
2025-02-08 17:44:34 +0100

    chore: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,23 @@
+import { getBlogsSitemap } from "@/api/blogs";
 import { getCertificateOptions } from "@/api/certificates";
 import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const host = "pomen.org";
   const certificateOptions = await getCertificateOptions();
+  const blogForSitemap = await getBlogsSitemap();
 
   const certificates: MetadataRoute.Sitemap = certificateOptions.map((opt) => ({
     url: `${host}/profil/${opt.slug}`,
     changeFrequency: "monthly",
     priority: 1,
+    images: [opt.image]
+  }));
+
+  const blogs: MetadataRoute.Sitemap = blogForSitemap.map((opt) => ({
+    url: `${host}/blogs/${opt.slug}`,
+    changeFrequency: "monthly",
+    priority: 0.5,
     images: [opt.image]
   }));
 
@@ -33,6 +42,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `${host}/blog`,
       changeFrequency: "yearly",
       priority: 0.5
-    }
+    },
+    ...blogs
   ];
 }
